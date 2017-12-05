13efd792d6b9bd6429c06093ea4f80e3cf3e6b09
My sweet heart <My sweet heart@192.168.1.174>
2017-12-05 12:36:47 -0600

    odd or even printed

---
+++ sort/src/sort/oddeven.java
@@ -0,0 +1,25 @@
+package sort;
+
+import java.util.Scanner;
+
+public class oddeven {
+
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ENTER THE NUMBER:");
+		Scanner sc =new Scanner(System.in);
+		int i = sc.nextInt();
+		if(i%2==0)
+			System.out.println("The number is even");
+		
+		else
+			
+			System.out.println("odd");
+		
+
+	}
+
+}
